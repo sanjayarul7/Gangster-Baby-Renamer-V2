6d6e3e88b99d416c2933ecc063246cb6f79f316c
sanjayarul7 <Sanjayvillian1@gmail.com>
2024-12-26 23:38:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", "22928570"))
 
 API_HASH = os.environ.get("API_HASH", "60bb37bddecb48c27c3e86906a077603")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQGFzVUAxsUqrhGS1aBnjwQbO0ucEFP75lvhObyrZegzX8jmZrCInu-pSrTu97qEuqej1DD2zd2w7VoNP2-rdU57HPNXfXvQChJztWUFJygOUXnFqhLHIEuOgI9sC91wZxwWwrC1grOyw66y1E9ubWxgbo9kZsSyhpgnihsRCkFBFTCvd3X-yAt6JyMLUWpZVl_daD99g59Orso9duK7EYKvDrK_pY34jAvL5yud7tfYgt0CrPjWDBK6o0zyv3O1xlAhagXAPwQW50wZfVbVzGedhBB5I_oTdPJsh5MuEaNRT5dQS7SuBf0F08AAYQPfZgAAaxTnOtjcIwr8pGWKkUeogRoiHAAAAAB3zmrgAA")
 
 
 
