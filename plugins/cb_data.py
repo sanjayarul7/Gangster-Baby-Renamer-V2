8069aa42f93cb0bda580953c8ba99496b6ebd8f2
sanjayarul7 <Sanjayvillian1@gmail.com>
2024-12-27 17:56:02 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -16,15 +16,15 @@ from helper.progress import humanbytes
 from helper.set import escape_invalid_curly_brackets
 import os
 
-log_channel = int(os.environ.get("LOG_CHANNEL", ""))
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1002446423158"))
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22928570"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "60bb37bddecb48c27c3e86906a077603")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQGFzVUAxsUqrhGS1aBnjwQbO0ucEFP75lvhObyrZegzX8jmZrCInu-pSrTu97qEuqej1DD2zd2w7VoNP2-rdU57HPNXfXvQChJztWUFJygOUXnFqhLHIEuOgI9sC91wZxwWwrC1grOyw66y1E9ubWxgbo9kZsSyhpgnihsRCkFBFTCvd3X-yAt6JyMLUWpZVl_daD99g59Orso9duK7EYKvDrK_pY34jAvL5yud7tfYgt0CrPjWDBK6o0zyv3O1xlAhagXAPwQW50wZfVbVzGedhBB5I_oTdPJsh5MuEaNRT5dQS7SuBf0F08AAYQPfZgAAaxTnOtjcIwr8pGWKkUeogRoiHAAAAAB3zmrgAA")
 
-ADMIN = os.environ.get("ADMIN", "")
+ADMIN = os.environ.get("ADMIN", "2010016480")
 
 app = Client("test", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
