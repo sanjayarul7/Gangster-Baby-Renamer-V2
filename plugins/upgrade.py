62f1d8c05c3129659a0b5b05dcb9bff31af12415
sanjayarul7 <Sanjayvillian1@gmail.com>
2024-12-27 18:10:58 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,20 +5,20 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
+	Daily  Upload limit 2GB
 	Price 0
 	
 	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Daily  Upload  limit 100GB
+	Price Rs 90  ind /🌎 1.2$  per Month
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Daily Upload limit 250GB
+	Price Rs 180  ind /🌎 2.5$  per Month
 	
 	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Daily Upload limit 500GB
+	Price Rs 270  ind /🌎 3.5$  per Month
 	
 	
 	Pay Using Upi I'd ```7808912076@paytm```
@@ -39,16 +39,16 @@ async def upgradecm(bot,message):
 	Price 0
 	
 	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 66  ind /🌎 0.8$  per Month
+	Daily  Upload  limit 100GB
+	Price Rs 90  ind /🌎 1.2$  per Month
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 100  ind /🌎 1.2$  per Month
+	Daily Upload limit 250GB
+	Price Rs 180  ind /🌎 2.5$  per Month
 	
 	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 206  ind /🌎 2.5$  per Month
+	Daily Upload limit 500GB
+	Price Rs 270  ind /🌎 3.5$  per Month
 	
 	
 	Pay Using Upi I'd ```7808912076@paytm```
